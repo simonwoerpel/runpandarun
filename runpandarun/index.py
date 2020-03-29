fa8b75352a5f0cd006004095afc9092bf5879e13
Simon Wörpel <simon.woerpel@medienrevolte.de>
2020-03-29 18:18:49 +0200

    This was annoying

--- runpandarun/index.py
+++ runpandarun/index.py
@@ -10,8 +10,9 @@ def apply_index(df, config):
         if banal.is_mapping(dt_index):
             if 'column' not in dt_index:
                 raise ConfigError('`dt_index` setting not valid. please specify at least `column`, either as mapping or as simple string value')  # noqa
-            col = dt_index.pop('column')
-            df.index = pd.DatetimeIndex(pd.to_datetime(df[col], **dt_index))
+            col = dt_index['column']
+            index_conf = {k: v for k, v in dt_index.items() if k != 'column'}
+            df.index = pd.DatetimeIndex(pd.to_datetime(df[col], **index_conf))
             del df[col]
             return df
         df.index = pd.DatetimeIndex(pd.to_datetime(df[dt_index]))
